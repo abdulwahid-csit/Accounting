95eb2266f463a8ce41e820e068969913f49ee5f8
Asim <asim@4iisolutions.com>
2024-09-19 06:14:12 -0700

    reports component

---
+++ src/app/modules/reports/BalanceSheet/BalanceSheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-BalanceSheet',
+  templateUrl: './BalanceSheet.component.html',
+  styleUrls: ['./BalanceSheet.component.css']
+})
+export class BalanceSheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/reports/Journal/Journal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Journal',
+  templateUrl: './Journal.component.html',
+  styleUrls: ['./Journal.component.css']
+})
+export class JournalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
