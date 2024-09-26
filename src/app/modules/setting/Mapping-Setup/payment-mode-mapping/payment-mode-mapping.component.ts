47aaeb4bef9f87905b53f6e55f2fd2f8f94c0c7f
Muhammad Ali <mali@4iisolutions.com>
2024-09-26 15:44:56 +0500

    APA-106

--- src/app/modules/setting/Mapping-Setup/payment-mode-mapping/payment-mode-mapping.component.ts
+++ src/app/modules/setting/Mapping-Setup/payment-mode-mapping/payment-mode-mapping.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import { CrudService } from 'src/app/shared/services/crud.service';
 
 @Component({
   selector: 'app-payment-mode-mapping',
@@ -7,10 +9,47 @@ import { BsModalService } from 'ngx-bootstrap/modal';
   styleUrls: ['./payment-mode-mapping.component.scss']
 })
 export class PaymentModeMappingComponent {
-  constructor(private modelservice:BsModalService){
+  settingForm!: FormGroup;
+  bankingData: any;
+  constructor(private modalService: BsModalService,private fb: FormBuilder,private CrudService: CrudService) { }
 
+  ngOnInit() {
+    this.initForm()
+    this.fetchBankingData();
+  }
+  initForm() {
+    const validation = {
+      // item:[null,Validators.compose([Validators.required])],
+      item:[""],
+      business:[""],
+      inventory_asset_account:[""],
+      income_account:[""],
+      expense_account:[""],
+    };
+    this.settingForm = this.fb.group(validation);
   }
   close(){
-    this.modelservice.hide();
+    this.modalService.hide();
+  }
+  fetchBankingData(page: number = 1) {
+    this.CrudService.read('banking').subscribe(
+      (response: any) => {
+        if (response?.data?.data?.payload) {
+          // Extracting the payload
+          this.bankingData = response.data.data.payload.map((item: any) => {
+            return {
+              _id: item._id,
+              name: item.name, // Use account name
+              account_number: item.account_number // Optional: Add account number if needed
+            };
+          });
+        } else {
+          console.error('Unexpected response format', response);
+        }
+      },
+      error => {
+        console.error('Error:', error);
+      }
+    );
   }
 }
